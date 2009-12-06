69021802c39e8015a3916d739436774ad9f4543f
zigorou <zigorou@cpan.org>
2009-12-07 00:20:56 +0900

    initial commit

---
+++ lib/SQL/Builder.pm
@@ -0,0 +1,33 @@
+package SQL::Builder;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+SQL::Builder -
+
+=head1 SYNOPSIS
+
+  use SQL::Builder;
+
+=head1 DESCRIPTION
+
+SQL::Builder is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
